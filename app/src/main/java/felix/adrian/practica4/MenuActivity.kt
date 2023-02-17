794b5f0a65e50d6aae3562cfde09d51cff6b1c03
Elmamis69 <guerofelix234@gmail.com>
2023-02-16 20:03:47 -0700

    Terminado

---
+++ app/src/main/java/felix/adrian/practica4/MenuActivity.kt
@@ -0,0 +1,11 @@
+package felix.adrian.practica4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
